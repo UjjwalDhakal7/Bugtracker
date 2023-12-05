889106835c39ad42579561a7373c75e1efbdd55e
unknown <ujjwaldkl07.com>
2023-12-05 22:02:06 +0545

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BugList from './views/BugList'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      < BugList/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/views/BugList/index.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+import Navbar from '../Navbar';
+
+
+function BugList() {
+  return (
+    <div className="main">
+        <Navbar/>
+    </div>
+  )
+}
+export default BugList;
